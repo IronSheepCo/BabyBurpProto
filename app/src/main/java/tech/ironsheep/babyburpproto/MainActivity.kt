c2a0d39671418d0c86581173aee5a6d48d25950c
Radu Dita <hades2510@yahoo.com>
2017-02-28 13:10:25 +0200

    added id for floating button and linked callback

--- app/src/main/java/tech/ironsheep/babyburpproto/MainActivity.kt
+++ app/src/main/java/tech/ironsheep/babyburpproto/MainActivity.kt
@@ -3,10 +3,15 @@ package tech.ironsheep.babyburpproto
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add_child.setOnClickListener {
+        }
     }
 }
