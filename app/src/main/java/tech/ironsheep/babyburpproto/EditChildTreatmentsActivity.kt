027e09064a50e6c3f4e42da2f01b4a85ff736356
Bogdan Serbanescu <armistitiu@gmail.com>
2017-03-08 16:33:42 +0200

    add edit child treatment activity

---
+++ app/src/main/java/tech/ironsheep/babyburpproto/EditChildTreatmentsActivity.kt
@@ -0,0 +1,25 @@
+package tech.ironsheep.babyburpproto
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import kotlinx.android.synthetic.main.activity_edit_child_treatments.*
+import tech.ironsheep.babyburpproto.models.Child
+import tech.ironsheep.babyburpproto.models.Storage
+
+class EditChildTreatmentsActivity : AppCompatActivity() {
+    private lateinit var CurrentChild: Child
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_child_treatments)
+        setSupportActionBar(toolbar)
+        val childIndex = intent.getIntExtra("childIndex", 0)
+        CurrentChild = Storage.data.children[childIndex]
+        titleName.text = CurrentChild.name
+
+    }
+}
