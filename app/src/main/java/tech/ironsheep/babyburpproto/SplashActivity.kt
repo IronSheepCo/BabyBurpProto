e918438bbb5b925868c59732b43b47211b790b2b
mongoose76 <doru@snowglobegames.com>
2017-03-08 18:20:35 +0200

    Improved splash screen

--- app/src/main/java/tech/ironsheep/babyburpproto/SplashActivity.kt
+++ app/src/main/java/tech/ironsheep/babyburpproto/SplashActivity.kt
@@ -1,21 +1,25 @@
 package tech.ironsheep.babyburpproto
 
+import android.app.Activity
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
 import kotlin.concurrent.thread
 
-
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContentView(R.layout.activity_splash);
+
+        // Start the app main activity after a 1 second delay
         thread(start=true){
             SystemClock.sleep(1000)
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+
+            // close this activity
             finish()
         }
     }
