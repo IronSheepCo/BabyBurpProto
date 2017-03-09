4c2ce2eeefefaf939001dd1eeabb11e2a1a1bb67
Bogdan Serbanescu <armistitiu@gmail.com>
2017-03-09 17:55:12 +0200

    EditChildTreatmentsActivity.kt refresh treatment list. Start AddTreatmentActivity.kt layout

--- app/src/main/java/tech/ironsheep/babyburpproto/AddTreatmentActivity.kt
+++ app/src/main/java/tech/ironsheep/babyburpproto/AddTreatmentActivity.kt
@@ -6,17 +6,21 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
+import kotlinx.android.synthetic.main.activity_add_treatment.*
+import tech.ironsheep.babyburpproto.models.Storage
 
-class AddTreatmentActivity : AppCompatActivity() {
+class AddTreatmentActivity : MenuActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_treatment)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar?
-        setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton?
-        fab!!.setOnClickListener { view ->
+        // set child name as page title
+        val childName = intent.getStringExtra("childName")
+        //CurrentChild = Storage.data.children[childIndex]
+        title = childName + " - Adauga tratament"
+
+        fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
--- app/src/main/java/tech/ironsheep/babyburpproto/EditChildTreatmentsActivity.kt
+++ app/src/main/java/tech/ironsheep/babyburpproto/EditChildTreatmentsActivity.kt
@@ -1,9 +1,13 @@
 package tech.ironsheep.babyburpproto
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_edit_child_treatments.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_edit_child_treatments.*
 import tech.ironsheep.babyburpproto.models.Child
 import tech.ironsheep.babyburpproto.models.Storage
 
@@ -18,6 +22,40 @@ class EditChildTreatmentsActivity : MenuActivity() {
         val childIndex = intent.getIntExtra("childIndex", 0)
         CurrentChild = Storage.data.children[childIndex]
         title = "Detalii tratament: " + CurrentChild.name
+        //populate child treatment list
+        refreshTreatmentList()
+        addTreatment.setOnClickListener {
+            val intent = Intent(this, AddTreatmentActivity::class.java)
+            intent.putExtra("childName", CurrentChild.name)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+    /**
+     * Populates the treamtents list
+     */
+    private fun refreshTreatmentList() {
+
+        // select treatment
+        val treatmentNames = CurrentChild.treatments.map { it -> it.name}
+
+        // create adapter
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, treatmentNames)
+        treatmentListView.setAdapter(adapter)
+
+        // set click listener
+        treatmentListView.setOnItemClickListener { adapterView, view, index, l ->
+            run {
+                val intent = Intent(this, EditChildTreatmentsActivity::class.java)
+                intent.putExtra("treatmentIndex", index)
+                startActivity(intent)
+            }
+        }
     }
 
     /**
